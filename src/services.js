71b197aa67357be4dae0cada825e62351842c0cc
Siva Nandu S <sivanandus2003@gmail.com>
2023-06-03 13:22:39 +0530

    Server url correction

--- src/services.js
+++ src/services.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
-const FRONT_URL = 'http://localhost:3000';
+const BASE_URL = 'https://product-based-backend.onrender.com';
+const FRONT_URL = 'https://siva-nandu-s.github.io/Production-Based-Siva-Nandu-S/';
 
 export {BASE_URL, FRONT_URL};
