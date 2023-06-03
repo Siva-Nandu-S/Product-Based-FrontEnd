5598796fed6942498ea4f19b65bff90188d0cc82
Siva Nandu S <sivanandus2003@gmail.com>
2023-06-03 19:06:00 +0530

    Corrected the FRONT_URL

--- src/services.js
+++ src/services.js
@@ -1,4 +1,4 @@
 const BASE_URL = 'https://product-based-backend.onrender.com';
-const FRONT_URL = 'https://product-based-siva-nandu-s.netlify.app/login';
+const FRONT_URL = 'https://product-based-siva-nandu-s.netlify.app';
 
 export {BASE_URL, FRONT_URL};
