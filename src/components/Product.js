e4204850c3b959184bb4626035efb5c9f6aac255
Siva Nandu S <sivanandus2003@gmail.com>
2023-06-04 08:48:33 +0530

    Error correction in /products/product:id

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { BASE_URL,FRONT_URL } from "../services";
+import { useParams, Link } from "react-router-dom";
+import { BASE_URL } from "../services";
 
 const Product = () => {
   const id = useParams();
   const URL = BASE_URL;
-  const F_URL = FRONT_URL;
 
   const [product, setProduct] = useState([]);
 
@@ -22,14 +21,14 @@ const Product = () => {
     setProduct(data);
   };
 
-  let link = `${F_URL}/purchase/`;
+  let link = `/purchase/`;
 
   return (
     <div className="product-container">
       <h1>{product.name}</h1>
       <h4>Price : {product.price}</h4>
       <h4>Remaining Stock : {product.stock}</h4>
-      <a href={link + id.id} className="product-buy-button">Buy</a>
+      <Link to={link + id.id} className="product-buy-button">Buy</Link>
     </div>
   );
 };
