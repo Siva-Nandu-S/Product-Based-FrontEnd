a54bca053eaa75d15e206b487a1ac28ff716b6e3
Siva Nandu S <sivanandus2003@gmail.com>
2023-08-02 22:35:17 +0530

    changed server

--- src/services.js
+++ src/services.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://product-based-backend.onrender.com';
-const BASE_URL = 'http://localhost:3001'
+const BASE_URL = 'https://product-based-backend.onrender.com';
+// const BASE_URL = 'http://localhost:3001'
 const FRONT_URL = 'https://product-based-siva-nandu-s.netlify.app';
 
 export {BASE_URL, FRONT_URL};
